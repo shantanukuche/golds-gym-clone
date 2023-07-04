a6ff12ee89721778eb514438c9705bd9994a7342
shantanu kuche <1010shantanukuche@gmail.com>
2023-07-04 11:02:44 +0530

    updated .env file in gitignore

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': '93e251d376msh305533898ded419p1fe877jsnefe913e2460d',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
